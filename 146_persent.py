9220571bab0718cbb23cbb4cac0718a67b15a265
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-04-14 14:36:43 +0300

    Update 146_persent.py

--- 146_persent.py
+++ 146_persent.py
@@ -1,13 +1,15 @@
 def MassVote(N, Votes):
     Copy = sorted(Votes, reverse=True)
     winner = Copy[0]
-    if Copy[1] == winner:
+    if N != 1 and Copy[1] == winner:
         return 'no winner'
     S = 0
     for i in range(N):
         S += Votes[i]
         if Votes[i] == winner:
             K = i+1
+    if S == 0:
+        return 'no winner'
     if winner / S > 0.5:
         res1 = 'majority '
     else:
