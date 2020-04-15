67a97e34124d10538adf0b8afa7cc334716e7800
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-04-15 15:07:30 +0300

    Create Yandex_taxi.py

---
+++ Yandex_taxi.py
@@ -0,0 +1,16 @@
+def Unmanned(L, N, track):
+    S = 0
+    t = 0    
+    for j in range(N): 
+        flag = False      
+        track[j].append(flag)
+    while S < L:          
+        Step = True     
+        for j in range(N): 
+            track[j][4] = not ((t % (track[j][1] + track[j][2])) < track[j][1])   
+            if S == track[j][0]:        
+                Step = Step and track[j][4]
+        if Step:
+            S += 1  
+        t += 1
+    return t
