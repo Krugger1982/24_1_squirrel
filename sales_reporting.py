27fdd3c46ef0f8700a239fea1ef71723a6a18226
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-04-17 17:26:30 +0300

    Create sales_reporting.py

---
+++ sales_reporting.py
@@ -0,0 +1,28 @@
+def ShopOLAP(N, items):
+    S = []
+    for i in range(N):
+        position = items[i].split(' ')
+        S.append(position)
+    for i in range(len(S)):
+    # summing the same items in the sales list
+        for j in range(-1, -len(S)-1, -1):
+            if (S[i][0] == S[j][0]) and (j != (i - len(S))):
+                S[i][1] = str(int(S[i][1]) + int(S[j][1]))
+                S[j][1] = 0
+    for i in range(-1, -len(S)-1, -1):
+    # deleting empty elements in the sales list
+        if S[i][1] == 0:
+            del S[i]
+    k = 1
+    while k > 0 :    
+        k = 0       
+        for i in range (len(S)-1) :
+        # "The bubble" sorting
+            if S[i][1] < S[i+1][1] :     
+                S[i], S[i+1] = S[i+1], S[i]   
+                k += 1   
+            elif S[i][1] == S[i+1][1] and S[i][0] > S[i+1][0]:
+                S[i], S[i+1] = S[i+1], S[i]
+    for i in range(len(S)):
+        S[i] = ' '.join(S[i])
+    return S
