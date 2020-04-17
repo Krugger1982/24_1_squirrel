bbb252d3ad7cb1b2b22fa397b53e7a35a0885a24
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-04-17 18:42:28 +0300

    Update sales_reporting.py

--- sales_reporting.py
+++ sales_reporting.py
@@ -8,11 +8,9 @@ def ShopOLAP(N, items):
         for j in range(-1, -len(S)-1, -1):
             if (S[i][0] == S[j][0]) and (j != (i - len(S))):
                 S[i][1] = str(int(S[i][1]) + int(S[j][1]))
-                S[j][1] = 0
-    for i in range(-1, -len(S)-1, -1):
-    # deleting empty elements in the sales list
-        if S[i][1] == 0:
-            del S[i]
+                S[j][1] = '0'
+    M = len(S) - 1
+
     k = 1
     while k > 0 :    
         k = 0       
@@ -23,7 +21,13 @@ def ShopOLAP(N, items):
                 k += 1   
             elif S[i][1] == S[i+1][1] and S[i][0] > S[i+1][0]:
                 S[i], S[i+1] = S[i+1], S[i]
-                k += 1
+                k +=1
+    while M > 0:
+    # deleting empty elements in the sales list
+        if S[M][1] == '0':
+            del S[M]
+        M -= 1
     for i in range(len(S)):
         S[i] = ' '.join(S[i])
+
     return S
