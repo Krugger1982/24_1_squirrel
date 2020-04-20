0b52aea7162b5cc8512383a75c1dbcb2cc92849d
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-04-20 11:49:06 +0300

    Update Lapot1.py

--- Lapot1.py
+++ Lapot1.py
@@ -2,7 +2,7 @@ position = [['', 1]]
 N = 0
 
 def add(arg, string):
-    return string + arg
+    return string + arg        
 
 def delete(arg, string):
     if arg < len(string):
@@ -11,7 +11,7 @@ def delete(arg, string):
         string = ''
     return string
         
-def ind(N, arg, string):
+def ind(arg, string):
     if arg < len(string):
         return string[arg]
     else:
@@ -21,12 +21,12 @@ def BastShoe(command):
     global position
     global N
     S = []  
-    S.append(int(command[0]))
+    S.append(int(command[0]))         
     S.append(command[2:])
     if S[0] == 1 and N < len(position)-1:
-        del position[:N] 
-        del position[1:] 
-        N = 0           
+        del position[:N]  # обнуляем цепочку перед этой командой (больше назад не откатишь)
+        del position[1:]  # и цепочку после этой команды (вперед тоже не откатишь, начали новую ветку)
+        N = 0             # В массиве остался единственный элемент - предыдущее состояние        
         position.append([add(S[1], position[N][0]), S[0]])
         N += 1
     elif S[0] == 1:
@@ -35,18 +35,18 @@ def BastShoe(command):
     elif S[0] == 2 and S[1].isdigit() and N < len(position)-1:
         line = delete(N, int(S[1]), position[N][0])
         position.append([line, S[0]])
-        del position[:N]  
-        del position[1:]  
-        N = 1               
+        del position[:N]  # обнуляем цепочку перед этой командой (больше назад не откатишь)
+        del position[1:]  # и цепочку после этой команды (вперед тоже не откатишь, начали новую ветку)
+        N = 1             # В массиве остался единственный элемент - предыдущее состояние        
     elif S[0] == 2 and S[1].isdigit():
         line = delete(N, int(S[1]), position[N][0])
         position.append([line, S[0]])
         N += 1
     elif S[0] == 3 and S[1].isdigit():
-        line = ind(N, int(S[1]), position[N][0])
+        line = ind(int(S[1]), position[N][0])
+        del position [:N+1]
         N = 0
-        del position [:]
-        position.append([line, S[0]])
+        position.insert(0, [line, S[0]])
     elif S[0] == 4 and S[1] == '' and (position[N][1] == 1 or position[N][1] == 2) and (N > 0):
         N -= 1
     elif S[0] == 5 and S[1] == '' and N < (len(position)-1):
