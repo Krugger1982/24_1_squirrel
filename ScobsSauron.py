7f46a81b4302973fd0e4ffcf39d7cf16010533d7
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-04-27 17:41:42 +0300

    Update ScobsSauron.py

--- ScobsSauron.py
+++ ScobsSauron.py
@@ -1,24 +1,26 @@
-def BalancedParentheses(N):
+def Brackets(N):
     result = []
     if N == 1:
         return ['()']
     elif N == 2:
-        res = BalancedParentheses(N-1)
+        res = Brackets(N-1)
         for i in range (len(res)):
             result.append('()' + res[i])
             result.append(res[i] + '()')
             result.append('(' + res[i] + ')')
     else:
-        res = BalancedParentheses(N-1)
-        res1 = BalancedParentheses(N-2)
+        res = Brackets(N-1)
+        res1 = Brackets(2)
         for i in range (len(res)):
             result.append('()' + res[i])
             result.append(res[i] + '()')
             result.append('(' + res[i] + ')')        
             for i in range(len(res1)):
                 result.append('(' * (N-2) + ')' * (N-2) + res1[i])
-    n = []                     # Удаляем одинаковые элементы
+    n = []                      
     for i in result:
         if i not in n:
             n.append(i)
-    return ' '.join(n)
+    return n
+def BalancedParentheses(N):
+    return ' '.join(Brackets(N))
