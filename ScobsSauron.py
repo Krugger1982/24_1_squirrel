1bdd1b62b5a9241a3531fbf5a882a942cce011b9
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-04-27 19:47:41 +0300

    Update ScobsSauron.py

--- ScobsSauron.py
+++ ScobsSauron.py
@@ -1,26 +1,26 @@
-def Brackets(N):
-    result = []
-    if N == 1:
-        return ['()']
-    elif N == 2:
-        res = Brackets(N-1)
-        for i in range (len(res)):
-            result.append('()' + res[i])
-            result.append(res[i] + '()')
-            result.append('(' + res[i] + ')')
+def gen_breckets(N, prefix='', A = []):
+    if N == 0:
+        A.append(prefix)
     else:
-        res = Brackets(N-1)
-        res1 = Brackets(2)
-        for i in range (len(res)):
-            result.append('()' + res[i])
-            result.append(res[i] + '()')
-            result.append('(' + res[i] + ')')        
-            for i in range(len(res1)):
-                result.append('(' * (N-2) + ')' * (N-2) + res1[i])
-    n = []                      
-    for i in result:
-        if i not in n:
-            n.append(i)
-    return n
+        gen_breckets(N-1, prefix + '(')
+        gen_breckets(N-1, prefix + ')')
+    return A
+
 def BalancedParentheses(N):
-    return ' '.join(Brackets(N))
+    M = 2*N
+    A = gen_breckets(M)
+    B = []
+    for i in range(len(A)):
+        Ability = True
+        Count1 = 0
+        Count0 = 0
+        for j in range(len(A[i])):
+            if A[i][j] == '(':
+                Count1 += 1
+            else:
+                Count0 += 1
+            if Count0 > Count1:
+                Ability = False
+        if Ability and A[i][0] == '(' and A[i][-1] == ')' and Count1 == Count0:
+            B.append(A[i])
+    return ' '.join(B)
